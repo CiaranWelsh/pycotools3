fb7a6343a15f93db79ce7a870930af2ffed5e031
CiaranWelsh <c.welsh2@newcastle.ac.uk>
2017-01-25 09:36:40 +0000

    Added PyCoTools subfolder.
    Build and publish onto pypi directly from PyCoTools directory on my
    system, rather than copying to another location to do it

--- setup.py
+++ setup.py
@@ -1,8 +1,33 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Sep 12 20:14:25 2016
 
-@author: b3053674
+ This file is part of PyCoTools.
+
+ PyCoTools is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ PyCoTools is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with PyCoTools.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+
+ Object:
+ 
+Setup tools for PyCoTools
+
+
+ $Author: Ciaran Welsh
+ $Date: 22-01-2017
+ Time:  12:29
+
 """
 
 from distutils.core import setup
@@ -10,40 +35,45 @@ import  setuptools
 
 
 #version
-MAJOR = 2
-MINOR = 1
-MICRO = 9
+MAJOR = 3
+MINOR = 0
+MICRO = 15
 #=======
 __version__ = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
-setuptools.setup(
+setup(
   name = 'PyCoTools',
   packages = ['PyCoTools'], # this must be the same as the name above
-  include_package_data=True,
-
   version = __version__,
   description = 'A python copasi toolbox',
   author = 'Ciaran Welsh',
   requires=['lxml','argparse','pandas','numpy','scipy','matplotlib.pyplot'],
   package_data={'PyCoTools':['*.py','Documentation/*.pdf',
-                             'Documentation/*.html',
-                             'Documentation/*.py',
-                             'Documentation/Examples/*.py',
-                             'Documentation/Examples/*.txt',
-                             'Licence.txt',
-                             'Tests/*.py']},#,'Examples':['*.pdf' ,'*.txt' ,'*.cps' ,'*.py', '*.jpeg' ,'*.png', '*.tiff']},
+                             'Documentation/*.html','Licence.txt',
+                             'ReadMe.md',
+                             'Examples/*.py',
+                             'Examples/*.cps',
+                             'Scripts/*.py',
+                             'Tests/*.py',
+                             'Tests/*.cps']},
   author_email = 'c.welsh2@newcastle.ac.uk',
-  url = 'https://github.com/CiaranWelsh/PyCoTools', # use the URL to the github repo
+  url = 'https://github.com/CiarnWelsh/PyCoTools', # use the URL to the github repo
 #  download_url = 'https://github.com/b3053674/pydentify/tarball/0.1',
   keywords = ['systems biology','modelling','biological','networks','copasi','identifiability analysis','profile likelihood'],
 #  include_package_data=False,
-  license='GPLv3',
+  license='GPL4',
 #  platform=['windows','linux'],
   install_requires=['pandas','numpy','scipy','matplotlib',
                     'lxml'],
-  long_description='''
-  
-  
-  
-  
-''')
+  long_description='''Tools for using Copasi via Python and conduction profile likelihoods. See Github page for more details''')
+
+
+
+
+
+
+
+
+
+
+
