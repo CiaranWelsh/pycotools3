7e37e554f2cc604f2844bd6ec9fa303423dc4fbf
Ciaran Welsh <c.welsh2@newcastle.ac.uk>
2017-06-09 15:14:19 +0100

    update MMF

--- PyCoTools/pycopi.py
+++ PyCoTools/pycopi.py
@@ -4430,10 +4430,13 @@ class RunMultiplePEs():
         LOG.info('Copying copasi file {} times'.format(self.kwargs['CopyNumber']))
         sub_copasi_files_dct={}
         copasi_path,copasi_filename=os.path.split(self.copasi_file)
-        for i in range(self.kwargs['CopyNumber']):
+        for i in range(1,self.kwargs['CopyNumber']-1):
             new_cps=os.path.join(copasi_path,copasi_filename[:-4]+'{}.cps'.format(str(i)))
             shutil.copy(self.copasi_file,new_cps)
             sub_copasi_files_dct[i]= new_cps
+        
+        sub_copasi_files_dct[0] = self.copasi_file
+        
         
         with open(self.copasi_file_pickle,'w')as f:
             pickle.dump(sub_copasi_files_dct,f)
