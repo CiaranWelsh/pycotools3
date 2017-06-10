e997e406d850011c040045eb32bfb4be980e3983
Ciaran Welsh <c.welsh2@newcastle.ac.uk>
2017-06-10 10:55:06 +0100

    bug fix

--- PyCoTools/pycopi.py
+++ PyCoTools/pycopi.py
@@ -4430,7 +4430,7 @@ class RunMultiplePEs():
         LOG.info('Copying copasi file {} times'.format(self.kwargs['CopyNumber']))
         sub_copasi_files_dct={}
         copasi_path,copasi_filename=os.path.split(self.copasi_file)
-        for i in range(1,self.kwargs['CopyNumber']-1):
+        for i in range(1,self.kwargs['CopyNumber']):
             new_cps=os.path.join(copasi_path,copasi_filename[:-4]+'{}.cps'.format(str(i)))
             shutil.copy(self.copasi_file,new_cps)
             sub_copasi_files_dct[i]= new_cps
--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ import  setuptools
 #version
 MAJOR = 3
 MINOR = 3
-MICRO = 69
+MICRO = 70
 
 #=======
 __version__ = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
