9dcfb957d42e917cf4da3e0a4b293a7678866f4a
Ciaran Welsh <c.welsh2@newcastle.ac.uk>
2017-06-22 09:04:36 +0100

    deleted folder that tox put into my PYCoTools dir

--- PyCoTools/Tests/test.py
+++ PyCoTools/Tests/test.py
@@ -10,7 +10,7 @@ data = os.path.join(dire,'TimeCourse1.txt')
 
 RMPE = PyCoTools.pycopi.RunMultiplePEs(f,data,Method = 'CurrentSolutionStatistics',
                                        RandomizeStartValues = 'false',
-                                       CopyNumber = 3,NumberOfPEs = 4)
+                                       CopyNumber = 5,NumberOfPEs = 4)
 
 RMPE.write_config_template()
 RMPE.set_up()
