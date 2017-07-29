6d1318e4723af1906c89a83c66f5aad622e06bb5
CiaranWelsh <c.welsh2@newcastle.ac.uk>
2017-07-29 20:39:41 +0100

    nearly finished pydentify.Plot class.

--- PyCoTools/PyCoToolsTutorial/Test/testing_kholodenko_manually.py
+++ PyCoTools/PyCoToolsTutorial/Test/testing_kholodenko_manually.py
@@ -7,24 +7,25 @@ Created on Sun Jul 23 16:47:47 2017
 """
 
 import site
-site.addsitedir('/home/b3053674/Documents/PyCoTools')
+#site.addsitedir('/home/b3053674/Documents/PyCoTools')
+site.addsitedir(r'C:\Users\Ciaran\Documents\PyCoTools')
 import PyCoTools
 import os, glob
 import numpy
 
-model = '/home/b3053674/Documents/PyCoTools/PyCoTools/PyCoToolsTutorial/Test/Kholodenko.cps'
-model2 = '/home/b3053674/Documents/PyCoTools/PyCoTools/PyCoToolsTutorial/Test/Kholodenko_1.cps'
-
-
-dire = '/home/b3053674/Documents/PyCoTools/PyCoTools/PyCoToolsTutorial/Test/MultipleParameterEstimationResults'
-
-pl_d='/home/b3053674/Documents/PyCoTools/PyCoTools/PyCoToolsTutorial/Test/ProfileLikelihood/0/(phosphorylation_of_MAPKK).KK3.txt'
-pl_f='/home/b3053674/Documents/PyCoTools/PyCoTools/PyCoToolsTutorial/Test/ProfileLikelihood/0/(phosphorylation_of_MAPKK).KK3.cps'
+#model = '/home/b3053674/Documents/PyCoTools/PyCoTools/PyCoToolsTutorial/Test/Kholodenko.cps'
+#model2 = '/home/b3053674/Documents/PyCoTools/PyCoTools/PyCoToolsTutorial/Test/Kholodenko_1.cps'
 #
 #
-
-results_d = '/home/b3053674/Documents/PyCoTools/PyCoTools/PyCoToolsTutorial/Test/Test4'
-results_f = '/home/b3053674/Documents/PyCoTools/PyCoTools/PyCoToolsTutorial/Test/test1/ParameterFit1.txt'
+#dire = '/home/b3053674/Documents/PyCoTools/PyCoTools/PyCoToolsTutorial/Test/MultipleParameterEstimationResults'
+#
+#pl_d='/home/b3053674/Documents/PyCoTools/PyCoTools/PyCoToolsTutorial/Test/ProfileLikelihood/0/(phosphorylation_of_MAPKK).KK3.txt'
+#pl_f='/home/b3053674/Documents/PyCoTools/PyCoTools/PyCoToolsTutorial/Test/ProfileLikelihood/0/(phosphorylation_of_MAPKK).KK3.cps'
+##
+##
+#
+#results_d = '/home/b3053674/Documents/PyCoTools/PyCoTools/PyCoToolsTutorial/Test/Test4'
+#results_f = '/home/b3053674/Documents/PyCoTools/PyCoTools/PyCoToolsTutorial/Test/test1/ParameterFit1.txt'
 
 
 
@@ -35,13 +36,13 @@ results_f = '/home/b3053674/Documents/PyCoTools/PyCoTools/PyCoToolsTutorial/Test
 import pandas
 import os,glob
 import site
-site.addsitedir(r'/home/b3053674/Documents/PyCoTools')
+#site.addsitedir(r'/home/b3053674/Documents/PyCoTools')
 import PyCoTools
 from PyCoTools.PyCoToolsTutorial import test_models
 
 TM = test_models.TestModels()
 kholodenko_string = TM.get_kholodenko_variant1()
-dire = '/home/b3053674/Documents/PyCoTools/PyCoTools/PyCoToolsTutorial'
+dire = r'C:\Users\Ciaran\Documents\PyCoTools\PyCoTools\PyCoToolsTutorial'
 kholodenko_model = os.path.join(dire, 'kholodenko_model.cps')
 
 with open(kholodenko_model, 'w') as f:
@@ -52,14 +53,14 @@ with open(kholodenko_model, 'w') as f:
 #print GMQ.get_local_kinetic_parameters_cns()    
     
     
-
-report= 'parameter_estimation_synthetic_data.txt'
-TC=PyCoTools.pycopi.TimeCourse(kholodenko_model,end=1000,intervals=10,step_size=100,
-                            report_name = report, global_quantities=None)
-
-
-## Give fake data a meaningful name
-data1 = TC['report_name']
+#
+#report= 'parameter_estimation_synthetic_data.txt'
+#TC=PyCoTools.pycopi.TimeCourse(kholodenko_model,end=1000,intervals=10,step_size=100,
+#                            report_name = report, global_quantities=None)
+#
+#
+### Give fake data a meaningful name
+#data1 = TC['report_name']
 
 
 #from PyCoTools.pycopi import RunMultiplePEs
@@ -99,7 +100,7 @@ data1 = TC['report_name']
 #'''
 #profile likelihood stuff
 #'''
-model = '/home/b3053674/Documents/PyCoTools/PyCoTools/PyCoToolsTutorial/kholodenko_model_1.cps'
+#model = '/home/b3053674/Documents/PyCoTools/PyCoTools/PyCoToolsTutorial/kholodenko_model_1.cps'
 #
 #PyCoTools.pydentify.ProfileLikelihood(model, parameter_path=RMPE['results_directory'],
 #                                      index = [0,1], run='slow', log10=True)
@@ -112,10 +113,46 @@ model = '/home/b3053674/Documents/PyCoTools/PyCoTools/PyCoToolsTutorial/kholoden
 
 
 
+#
+pl_dir = r'C:\Users\Ciaran\Documents\PyCoTools\PyCoTools\PyCoToolsTutorial\ProfileLikelihood'
+model = r'C:\Users\Ciaran\Documents\PyCoTools\PyCoTools\PyCoToolsTutorial\kholodenko_model_1.cps'
+parameter_path = r'C:\Users\Ciaran\Documents\PyCoTools\PyCoTools\PyCoToolsTutorial\MultipleParameterEstimationResults'
+#'/home/b3053674/Documents/PyCoTools/PyCoTools/PyCoToolsTutorial/ProfileLikelihood'
+PL = PyCoTools.pydentify.ParsePLData(model,pl_dir, parameter_path=parameter_path,
+                                     index=[0,1])
+keys = list(PL.data.keys())
 
-pl_dir = '/home/b3053674/Documents/PyCoTools/PyCoTools/PyCoToolsTutorial/ProfileLikelihood'
-PL = PyCoTools.pydentify.ParsePLData(pl_dir)
+for k in keys:
+    PyCoTools.pydentify.Plot2(PL.data, x=k, y='RSS')
+    
 
+
+
+'''
+
+Could have a class to plot the parameter of interest against
+the RSS. 
+Or make the class accept the list of parameters to plot
+in a single plot. 
+
+The independent values will be on the x axis (i.e. 1 to 10if the 
+parameter of interest scanned between 1 nd 10. 
+Then all other parameters will be on the y. 
+
+Arguments then --> x is a single parameter name
+                --> y is  single parameter or list of parameters 
+                            to put on the y axis. 
+)
+
+Need copasi file and parameter path as args
+
+
+'''
+
+
+#data = PL.data
+#print data
+#data.to_csv(os.path.join(os.path.dirname(pl_dir), 'test_pl_data.csv') )
 '''
 sort out PL boundaries when running PL
 '''
