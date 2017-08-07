15bdd2fa77c9b63d010de14702d30655d3c8a21a
CiaranWelsh <c.welsh2@newcastle.ac.uk>
2017-08-07 19:05:18 +0100

    last commit on develop before merge into coapsi versio 19

--- PyCoTools/Tests/get_model_quantities.py
+++ PyCoTools/Tests/get_model_quantities.py
@@ -29,7 +29,7 @@ import site
 site.addsitedir('C:\Users\Ciaran\Documents\PyCoTools')
 import PyCoTools
 from PyCoTools.PyCoToolsTutorial import test_models
-from PyCoTools.Tests import base_tests
+from PyCoTools.Tests import _test_base
 import unittest
 import glob
 import os
@@ -38,7 +38,7 @@ import pandas
 import random
 
 #
-class GetModelQuantitiesTests(base_tests._BaseTest):
+class GetModelQuantitiesTests(_test_base._BaseTest):
     def setUp(self):
         super(GetModelQuantitiesTests, self).setUp()
         
@@ -47,15 +47,52 @@ class GetModelQuantitiesTests(base_tests._BaseTest):
         """
         
         """
-        print self.GMQ.get_local_parameters()
+        sh = self.GMQ.get_local_parameters().shape
+        self.assertEqual(sh, (3,5))
 #        print self.GMQ.get_local_parameters()
+
+    def test_get_compartments(self):
+        sh = self.GMQ.get_compartments().shape
+        self.assertEqual(sh, (2,3))
+
+    def test_get_metabolites(self):
+        """
+
+        :return:
+        """
+        sh = self.GMQ.get_metabolites().shape
+        self.assertEqual(sh, (3,4))
+
+    def test_particle_to_conc_conversion1(self):
+        particle_number = 6.022140857e+20
+        vol_unit = 'ml'
+        quantity_unit = 'mmol'
+        conc = PyCoTools.pycopi.GetModelQuantities.convert_particles_to_molar(particle_number,
+                                                                              quantity_unit,
+                                                                              compartment_volume=1)
+        self.assertAlmostEqual(round(conc,0), 1)
+
+    def test_particle_to_conc_conversion2(self):
+        particle_number = 6.022140857e+20
+        vol_unit = 'ml'
+        quantity_unit = 'nmol'
+        conc = PyCoTools.pycopi.GetModelQuantities.convert_particles_to_molar(particle_number,
+                                                                              quantity_unit,
+                                                                              compartment_volume=1)
+        self.assertAlmostEqual(round(conc,0), 1000000)
+
+
+        # os.system('CopasiUI {}'.format(self.copasi_file))
+
+    def test_get_global_quantities(self):
+        """
+
+        :return:
+        """
+        print self.GMQ.get_global_quantities()
         
-        
-        
-        
-if __name__=='__main__':
+if __name__ == '__main__':
     unittest.main()
-        
         
         
         
