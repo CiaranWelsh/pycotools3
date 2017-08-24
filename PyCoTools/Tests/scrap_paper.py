0a1ebcb55f6bb46801d144bb0b14aa4d618e07ee
CiaranWelsh <c.welsh2@newcastle.ac.uk>
2017-08-24 14:15:14 +0100

    commit before pull to home from work
    Former-commit-id: 9cbefb0e704c41c761d4193fa66730de34f8079f

---
+++ PyCoTools/Tests/scrap_paper.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+'''
+ This file is part of PyCoTools.
+
+ PyCoTools is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ PyCoTools is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with PyCoTools.  If not, see <http://www.gnu.org/licenses/>.
+
+
+Author:
+    Ciaran Welsh
+Date:
+    12/03/2017
+
+ Object:
+'''
+
+
+import site
+site.addsitedir(r'C:\Users\Ciaran\Documents\PyCoTools')
+# site.addsitedir(r'/home/b3053674/Documents/PyCoTools')
+
+import PyCoTools
+from PyCoTools.PyCoToolsTutorial import test_models
+import unittest
+import glob
+import os
+import shutil
+import pandas
+from PyCoTools.Tests import _test_base
+import re
+from lxml import etree
+
+
+
+test_model = r'C:\Users\Ciaran\Documents\PyCoTools\PyCoTools\Tests\test_model.cps'
+
+test_report_name = os.path.join(os.path.dirname(test_model), 'testing_report.txt')
+model = PyCoTools.model.Model(test_model)
+
+test_ss_data = r'C:\Users\Ciaran\Documents\PyCoTools\PyCoTools\Tests\report4.txt'
+
+E = PyCoTools.pycopi.ExperimentMapper(model, test_ss_data, experiment_type='steadystate')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
