6447a58cf0f602b3defabc8d9758b8ad91b550a2
Ciaran Welsh <c.welsh2@newcastle.ac.uk>
2017-09-07 17:37:38 +0100

    started profile_likleihood tests

---
+++ pycotools/Tests/profile_likelihood_tests.py
@@ -0,0 +1,38 @@
+#-*-coding: utf-8 -*-
+"""
+
+ This file is part of pycotools.
+
+ pycotools is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ pycotools is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with pycotools.  If not, see <http://www.gnu.org/licenses/>.
+
+
+ $Author: Ciaran Welsh
+
+Module that tests the operations of the _Base base test
+
+"""
+
+import site
+site.addsitedir('C:\Users\Ciaran\Documents\pycotools')
+site.addsitedir('/home/b3053674/Documents/pycotools')
+import pandas
+import pycotools
+from pycotools.Tests import _test_base
+import unittest
+import os
+import pickle
+import test_data
+import numpy
+import shutil
+import glob
