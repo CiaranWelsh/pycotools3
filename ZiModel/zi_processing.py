fdadef7e0e23d3c3c139d7a60ae7b72d46b2cff7
Ciaran Welsh <c.welsh2@newcastle.ac.uk>
2017-10-01 10:00:33 +0100

    Replaced the cached_property constants with another function which also gets the local_parameters simulation type

--- ZiModel/zi_processing.py
+++ ZiModel/zi_processing.py
@@ -9,7 +9,7 @@ cps_file1 = r'/home/b3053674/Documents/pycotools/ZiModel/zi2012.cps'
 cps_file2 = r'/home/b3053674/Documents/pycotools/ZiModel/zi2012_2.cps'
 cps_file3 = r'/home/b3053674/Documents/pycotools/ZiModel/zi2012_3.cps'
 
-zi = model.Model(cps_file2)
+zi = model.Model(cps_file1)
 
 
 report= 'parameter_estimation_synthetic_data.txt'
@@ -21,14 +21,19 @@ TC=tasks.TimeCourse(
 pandas.read_csv(TC.report_name,sep='\t').head()
 
 data1 = TC.report_name
+
+misc.correct_copasi_timecourse_headers(data1)
 ## Set getetic algorithm parameters to low for speed of demonstration
-PE=tasks.ParameterEstimation(zi, data1, method='genetic_algorithm',
-                             population_size=15, number_of_generations=10)
-PE.write_config_file()
+# PE=tasks.ParameterEstimation(zi, data1, method='genetic_algorithm',
+#                              population_size=15, number_of_generations=10)
+# PE.write_config_file()
+#
+# PE.setup()
+# PE.model.open()
 
+print zi.constants2
 
-
-
+# zi.open()
 
 
 
